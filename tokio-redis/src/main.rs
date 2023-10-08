51eea3b7f67f948087f5e15b8858a71f8cf20893
Karuption <pederson26@gmail.com>
2023-10-08 18:34:58 -0500

    Init

---
+++ tokio-redis/src/main.rs
@@ -0,0 +1,16 @@
+use std::io::Bytes;
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await.expect("Cannot Connect to Server");
+
+    client.set("hello", "world".into()).await?;
+
+    let result = client.get("hello").await?;
+    println!("Got value from the server, {:?}", result);
+
+    Ok(())
+}
+
+async fn test() {}
