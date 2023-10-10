b02cc059daf572285245b2a849a69f0c7b4682fe
Karuption <pederson26@gmail.com>
2023-10-09 23:25:30 -0500

    added Get and Set commands.

--- tokio-redis/src/main.rs
+++ tokio-redis/src/main.rs
@@ -1,16 +1,46 @@
-use std::io::Bytes;
-use mini_redis::{client, Result};
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let mut client = client::connect("127.0.0.1:6379").await.expect("Cannot Connect to Server");
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379")
+        .await
+        .expect("unable to start TCP listener");
 
-    client.set("hello", "world".into()).await?;
-
-    let result = client.get("hello").await?;
-    println!("Got value from the server, {:?}", result);
-
-    Ok(())
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move { process(socket).await });
+    }
 }
 
-async fn test() {}
+async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+
+    let mut map = std::collections::HashMap::new();
+
+    let mut conn = Connection::new(socket);
+
+    while let Some(frame) = conn.read_frame().await.unwrap() {
+        let command = Command::from_frame(frame).unwrap();
+        println!("Received command: {:?}", command);
+
+        let res = match command {
+            Set(cmd) => {
+                map.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+
+            Get(cmd) => {
+                if let Some(value) = map.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+
+            cmd => panic!("not implemented {:?}", cmd),
+        };
+
+        conn.write_frame(&res).await.unwrap();
+    }
+}
