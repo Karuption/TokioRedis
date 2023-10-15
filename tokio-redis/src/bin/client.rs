ff85e30696d534ce1272ac4c089aa74ad986b650
Karuption <pederson26@gmail.com>
2023-10-15 11:21:57 -0500

    Client binary now using oneshot channel to communicate the responses to their request channel.

--- tokio-redis/src/bin/client.rs
+++ tokio-redis/src/bin/client.rs
@@ -1,10 +1,17 @@
 use bytes::{Buf, Bytes};
 use mini_redis::client;
-use tokio::sync::mpsc;
+use tokio::sync::{mpsc, oneshot};
 
 enum Command {
-    Get { key: String },
-    Set { key: String, val: Bytes },
+    Get {
+        key: String,
+        response: oneshot::Sender<mini_redis::Result<Option<Bytes>>>,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+        response: oneshot::Sender<mini_redis::Result<()>>,
+    },
 }
 
 #[tokio::main]
@@ -13,21 +20,29 @@ async fn main() {
 
     let tx1 = tx.clone();
     let t1 = tokio::spawn(async move {
+        let (res_tx, res_rx) = oneshot::channel();
         tx1.send(Command::Get {
             key: "foo".to_string(),
+            response: res_tx,
         })
         .await
-        .unwrap()
+        .unwrap();
+
+        println!("Response: {:?}", res_rx.await);
     });
 
     let tx2 = tx.clone();
     let t2 = tokio::spawn(async move {
+        let (res_tx, res_rx) = oneshot::channel();
         tx2.send(Command::Set {
             key: "foo".to_string(),
             val: "bar".into(),
+            response: res_tx,
         })
         .await
-        .unwrap()
+        .unwrap();
+
+        println!("Response: {:?}", res_rx.await);
     });
 
     let manager = tokio::spawn(async move {
@@ -37,11 +52,13 @@ async fn main() {
             use Command::*;
 
             match cmd {
-                Get { key } => {
-                    client.get(&key).await.unwrap();
+                Get { key, response } => {
+                    let res = client.get(&key).await;
+                    let _ = response.send(res);
                 }
-                Set { key, val } => {
-                    client.set(&key, val).await.unwrap();
+                Set { key, val, response } => {
+                    let res = client.set(&key, val).await;
+                    let _ = response.send(res);
                 }
             }
         }
